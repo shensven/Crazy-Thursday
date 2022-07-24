dc29d90fd7324e09be050fa59cb6356dd1b3e2c6
SvenFE <shensven2016@gmail.com>
2022-07-24 14:52:54 +0800

    chore: add @react-navigation & its deps

--- android/app/src/main/java/com/crazythursday/MainActivity.java
+++ android/app/src/main/java/com/crazythursday/MainActivity.java
@@ -1,11 +1,17 @@
 package com.crazythursday;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.zoontek.rnbars.RNBars; // <- add this necessary import
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
