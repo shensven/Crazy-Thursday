00d3a07c2a74db24b829f9fc579cd9e029a2d0b1
SvenFE <shensven2016@gmail.com>
2022-07-25 20:48:50 +0800

    chore: add @react-native-async-storage/async-storage

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,7 @@
 import {jest} from '@jest/globals';
 import 'react-native-gesture-handler/jestSetup';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.useFakeTimers();
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
