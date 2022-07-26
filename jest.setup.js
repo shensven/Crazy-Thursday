7c161b47b3a8356300a4f509183fc88cb892450b
SvenFE <shensven2016@gmail.com>
2022-07-26 14:37:25 +0800

    chore: add @react-native-clipboard/clipboard

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,9 @@
 import {jest} from '@jest/globals';
 import 'react-native-gesture-handler/jestSetup';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import mockClipboard from '@react-native-clipboard/clipboard/jest/clipboard-mock.js';
 
 jest.useFakeTimers();
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('@react-native-clipboard/clipboard', () => mockClipboard);
