071cdac3f9796c852b87dbc575f9c2825dc07100
SvenFE <shensven2016@gmail.com>
2022-07-26 19:46:58 +0800

    fix: update mock

--- jest.setup.js
+++ jest.setup.js
@@ -6,7 +6,13 @@ import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-
 
 jest.useFakeTimers();
 
-jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('@react-native-clipboard/clipboard', () => mockClipboard);
 jest.mock('react-native-device-info', () => mockRNDeviceInfo);
+jest.mock('./src/screens/Init');
