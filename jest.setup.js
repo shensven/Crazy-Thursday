ea24e3cc9140948e9bdfbe1fe9f502ed55f8e13d
SvenFE <shensven2016@gmail.com>
2022-07-29 12:03:45 +0800

    chore: drop @react-native-async-storage/async-storage

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 import {jest} from '@jest/globals';
 import 'react-native-gesture-handler/jestSetup';
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import mockClipboard from '@react-native-clipboard/clipboard/jest/clipboard-mock.js';
 import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
 
@@ -12,7 +11,6 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('@react-native-clipboard/clipboard', () => mockClipboard);
 jest.mock('react-native-device-info', () => mockRNDeviceInfo);
 jest.doMock('react-native-bootsplash', () => {
